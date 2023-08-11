0538a789c93079978bf64e4505975c628f07fce4
swtlcodes <107774768+swtlcodes@users.noreply.github.com>
2023-08-11 12:35:24 -0700

    Fixed issue where .JAR file was not working.

--- src/main/java/entities/enemies/minions/Blob.java
+++ src/main/java/entities/enemies/minions/Blob.java
@@ -20,9 +20,9 @@ public class Blob extends Enemy {
         blobImage = ImageIO.read(testUrl);
         ArrayList<Image> timmy = new ArrayList<>();
         timmy.add(gameWindow.player.player_gun_walk_1);
-        timmy.add(gameWindow.player.player_gun_walk_2);
-        timmy.add(gameWindow.player.player_gun_walk_3);
-        timmy.add(gameWindow.player.player_gun_walk_4);
+        timmy.add(gameWindow.player.player_shoot_walk_2);
+        timmy.add(gameWindow.player.player_stab_walk_3);
+        timmy.add(gameWindow.player.player_sword_walk_4);
         thing = new Animation(timmy, 100);
     }
 
